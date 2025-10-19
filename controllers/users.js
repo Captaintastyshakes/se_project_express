98f579935ce295e0da2c35185d64aa53960ff86b
Michael Weihmanngit config --global user.name Michael <mikej513@hotmail.com>
2025-10-19 12:46:17 -0600

    left a remnant 'uniError' handler from an ill-fated venture- have blasted it along with some remnant notes/error code I forgot to clean up.

--- controllers/users.js
+++ controllers/users.js
@@ -4,17 +4,7 @@ const User = require("../models/user");
 const { JWT_SECRET } = require("../utils/config");
 const error = require("../errors/errors");
 const cError = require("../errors/cErrors");
-//const uniError = require("../utils/cErrors");
-/*const {
-  ConflictError,
-  NotFoundError,
-  ForbiddenError,
-  UnauthorizedError,
-  BadRequestError,
-  GenericError,
-} = require("../utils/cErrors");*/
 
-//const cError {
 const {
   ConflictError,
   NotFoundError,
@@ -47,80 +37,46 @@ const signUp = (req, res, next) => {
         _id: user._id,
       });
     })
-    /*.catch((err) => {
-      if (!error[err.name]) {
-        thisErr = error.undefined;
-        const { code, message } = thisErr;
-        const outMessage = `Int code SU2: ${message} ${err.message}`;
-        return res.status(code).send({ message: outMessage });
-      }
-      thisErr = error[err.name];
-      const { code, message } = thisErr;
-      const outMessage = `Int code SU3: ${message} ${err.message}`;
-      return res.status(code).send({ message: outMessage });
-    });*/
     .catch((err) => {
       if (!error[err.name]) {
         thisErr = error.undefined;
-        return next(new uniError(`${thisErr}`, thisErr.code, thisErr.message));
+        return next(new Error(thisErr.message));
       }
       thisErr = error[err.name];
-      return next(new uniError(`${thisErr}`, thisErr.code, thisErr.message));
+      return next(new GenericError(thisErr.code, thisErr.message));
     });
 };
 
 const login = (req, res, next) => {
   let thisErr;
   const { email, password } = req.body;
-  return (
-    User.findUserByCredentials(email, password)
-      .then((user) => {
-        const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-          expiresIn: "7d",
-        });
-        const approvedData = {
-          avatar: user.avatar,
-          name: user.name,
-          _id: user._id,
-          //user,
-        };
-        return res.send({ token, approvedData });
-        //return res.send({ token, user });
-      })
-      /*.catch((err) => {
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      const approvedData = {
+        avatar: user.avatar,
+        name: user.name,
+        _id: user._id,
+      };
+      return res.send({ token, approvedData });
+    })
+    .catch((err) => {
+      if (cError[err.name]) {
+        return next(new cError[err.name](err.message));
+      }
       if (!error[err.name]) {
         thisErr = error.undefined;
-        const { code, message } = thisErr;
-        const outMessage = `Int code LI1: ${message} ${err.message}`;
-        return res.status(code).send({ message: outMessage });
+        return next(new Error(thisErr.message));
       }
       if (err.message.includes("FUBC")) {
         thisErr = error.ReferenceError;
-        const { code, message } = thisErr;
-        const outMessage = `Int code LI2: ${message} ${err.message}`;
-        return res.status(code).send({ message: outMessage });
+        return next(new UnauthorizedError(thisErr.message));
       }
       thisErr = error[err.name];
-      const { code, message } = thisErr;
-      const outMessage = `Int code LI3: ${message} ${err.message}`;
-      return res.status(code).send({ message: outMessage });
-    });*/
-      .catch((err) => {
-        if (cError[err.name]) {
-          return next(new cError[err.name](err.message));
-        }
-        if (!error[err.name]) {
-          thisErr = error.undefined;
-          return next(new Error(thisErr.message));
-        }
-        if (err.message.includes("FUBC")) {
-          thisErr = error.ReferenceError;
-          return next(new UnauthorizedError(thisErr.message));
-        }
-        thisErr = error[err.name];
-        return next(new GenericError(thisErr.code, thisErr.message));
-      })
-  );
+      return next(new GenericError(thisErr.code, thisErr.message));
+    });
 };
 
 const getCurrentUser = (req, res, next) => {
